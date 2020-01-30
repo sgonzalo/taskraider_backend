27998de9ddea6a8bfd4d044c3bfdcbbd36e7047c
sgonzalo <santiago.gonzalo360@gmail.com>
2020-01-30 22:38:45 +0000

    1st push

--- src/main.py
+++ src/main.py
@@ -7,9 +7,8 @@ from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from models import db
+from models import db, Employee
 #from models import Person
-
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
@@ -17,26 +16,40 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
-
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
-
 # generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
+@app.route('/employee', methods=['POST', 'GET'])
+def get_Employee():
+#Create a Employee and retrieve all Employees!!
+    if request.method == 'POST':
+        body = request.get_json()
+        if body is None:
+            raise APIException("You need to specify the request body as a json object", status_code=400)
+        if "full_name" not in body:
+            raise APIException('You need to specify the full_name', status_code=400)
+        if 'email' not in body:
+            raise APIException('You need to specify the email', status_code=400)
+        if 'password' not in body:
+            body['password'] = None
+        employee1 = Employee(full_name=body['full_name'], email = body['email'], password = body['password'])
+        db.session.add(employee1)
+        db.session.commit()
+        return "ok", 200
+    
+    # GET request
+    if request.method == 'GET':
+        all_employee = Employee.query.all()
+        all_employee = list(map(lambda x: x.serialize(), all_employee))
+        return jsonify(all_employee), 200
+    return "Invalid Method", 404
 
-@app.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
-
-    response_body = {
-        "hello": "world"
-    }
-
-    return jsonify(response_body), 200
-
+@app.route('/employee/<int:employee_id>', methods= ['PUT', 'GET', 'DELETE'])
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
--- src/models.py
+++ src/models.py
@@ -1,17 +1,25 @@
 from flask_sqlalchemy import SQLAlchemy
-
 db = SQLAlchemy()
-
 # class Person(db.Model):
 #     id = db.Column(db.Integer, primary_key=True)
 #     username = db.Column(db.String(80), unique=True, nullable=False)
 #     email = db.Column(db.String(120), unique=True, nullable=False)
-
 #     def __repr__(self):
 #         return '<Person %r>' % self.username
-
 #     def serialize(self):
 #         return {
 #             "username": self.username,
 #             "email": self.email
 #         }
+class Employee(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(120), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=True, nullable=False)
+    def serialize(self):
+        return {
+            "full_name": self.full_name,
+            "email": self.email,
+            "password": self.password,
+            "id": self.id
+        }
